db816810d9d252de30c64f80ba73f2f99df2da53
Dmitriy Shin <dimashin9@gmail.com>
2022-02-23 12:18:40 -0500

    finished most_common_word function

--- 50-ten-minutes-exercises/List and Tuples/most_repeating_word.py
+++ 50-ten-minutes-exercises/List and Tuples/most_repeating_word.py
@@ -1,13 +1,38 @@
 #!/usr/bin/env python
 
+from collections import Counter
+
+
 def most_repeating_word(lst: list) -> str:
-    for word in lst:
-        print(word)
+    """
+    Outputs a string with most repeated number of characters
+    """
+    return max(lst, key=lambda word: Counter(word).most_common(1)[0][1])
+
+    ### OLD SOLUTION ###
+    # sorted_lst = sorted(lst, key=len, reverse=True)
+    # output = []
+
+    # for i in range(len(sorted_lst)):
+    #     word = sorted_lst[i]
+    #     d = {}
+
+    #     for char in word:
+    #         d[char] = word.count(char)
+
+    #     output.append(d)
+
+    # output = [sum([value if value > 1 else 0 for value in d.values()])
+    #           for d in output]
+    # idx_max = output.index(max(output))
+
+    # return sorted_lst[idx_max]
 
 
 def main():
-    words = ['this', 'is', 'an', 'elementary', 'test', 'example']
-    most_repeating_word(words)  # elementary
+    words = ['this', 'is', 'an', 'elementary',
+             'test', 'example', 'anagram']
+    print(most_repeating_word(words))  # elementary
 
 
 if __name__ == '__main__':
