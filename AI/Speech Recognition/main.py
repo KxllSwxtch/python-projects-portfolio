07866f793963d0e0586776a670d8b5d4e445c667
KxllSwxtch <dimashin9@gmail.com>
2023-06-07 20:34:37 +0900

    added AI folder, added transcription script

---
+++ AI/Speech Recognition/main.py
@@ -0,0 +1,34 @@
+import whisper
+
+
+def speech_recognition(model='base'):
+    speech_model = whisper.load_model(model)
+    result = speech_model.transcribe('assets/Магазин - YUNGWAY feat BUSHIDO ZHO.mp3')
+
+    with open(f'transcription_{model}.txt', 'w') as file:
+        file.write(result['text'])
+
+
+def main():
+    models = {
+        1: 'tiny',
+        2: 'base',
+        3: 'small',
+        4: 'medium',
+        5: 'large'
+    }
+
+    for key, value in models.items():
+        print(f"{key}: {value}")
+
+    model = int(input('\nChoose AI model by entering a number from 1 to 5: '))
+
+    if model not in models.keys():
+        raise KeyError(f"Model {model} doesn't exist...")
+    
+    print('Transcription process has begun, please wait...')
+    speech_recognition(model=models[model])
+
+
+if __name__ == '__main__':
+    main()
